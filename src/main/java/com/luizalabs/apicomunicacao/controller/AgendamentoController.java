c1ca3c5c4870db03cb6761db80f6f3fcab27100c
Luan Lima <luanlima.olliveir@gmail.com>
2021-06-15 22:09:08 -0300

    Criando endpoint para consultar a situação do envio da comunicação.

--- src/main/java/com/luizalabs/apicomunicacao/controller/AgendamentoController.java
+++ src/main/java/com/luizalabs/apicomunicacao/controller/AgendamentoController.java
@@ -1,10 +1,14 @@
 package com.luizalabs.apicomunicacao.controller;
 
 import com.luizalabs.apicomunicacao.entity.dto.AgendamentoDTO;
+import com.luizalabs.apicomunicacao.entity.dto.LogEnvioMensagemDTO;
 import com.luizalabs.apicomunicacao.service.AgendamentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/agendamento")
@@ -17,5 +21,19 @@ public class AgendamentoController {
     @ResponseStatus(HttpStatus.OK)
     public AgendamentoDTO criarAgendamento(@RequestBody AgendamentoDTO agendamentoDTO) {
         return this.agendamentoService.criarAgendamento(agendamentoDTO).toDTO();
+    }
+
+    @GetMapping("{idAgendamento}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<LogEnvioMensagemDTO> consultarAgendamento(@PathVariable Integer idAgendamento) {
+        return this.agendamentoService.consultarAgendamento(idAgendamento).stream().
+                map(l -> LogEnvioMensagemDTO.builder().
+                        id(l.getId()).
+                        idAgendamento(l.getAgendamento().getId()).
+                        dataProcesaamento(l.getDataProcesaamento()).
+                        descricaoStatusEnvio(l.getStatusEnvio().getDescricao()).
+                        codigoStatus(l.getStatusEnvio().getCodigo()).
+                        build()).
+                collect(Collectors.toList());
     }
 }
