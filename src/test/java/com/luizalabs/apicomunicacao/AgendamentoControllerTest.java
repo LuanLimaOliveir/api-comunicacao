069512f47144c71964eae5867bcae4df03f7588f
Luan Lima <luanlima.olliveir@gmail.com>
2021-06-16 18:22:40 -0300

    Desenvolvendo teste de integração para consultar situação do agendamento.

--- src/test/java/com/luizalabs/apicomunicacao/AgendamentoControllerTest.java
+++ src/test/java/com/luizalabs/apicomunicacao/AgendamentoControllerTest.java
@@ -1,6 +1,8 @@
 package com.luizalabs.apicomunicacao;
 
 import com.luizalabs.apicomunicacao.controller.v1.AgendamentoController;
+import com.luizalabs.apicomunicacao.entity.LogEnvioMensagem;
+import com.luizalabs.apicomunicacao.entity.StatusEnvio;
 import com.luizalabs.apicomunicacao.entity.dto.AgendamentoDTO;
 import com.luizalabs.apicomunicacao.service.AgendamentoService;
 import org.junit.jupiter.api.Test;
@@ -11,10 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.core.Is.is;
@@ -58,5 +63,56 @@ public class AgendamentoControllerTest {
                 .andExpect(jsonPath("$.id", is(70)))
                 .andExpect(jsonPath("$.destinatario", is("luanlima.oliveir@gmail.com")))
                 .andExpect(jsonPath("$.idTipoComunicacao", is(1)));
+    }
+
+    @Test
+    public void consultarAgendamento() throws Exception {
+        AgendamentoDTO agendamentoDTO = AgendamentoDTO.builder()
+                .id(71)
+                .dataAgendamento(LocalDateTime.now())
+                .destinatario("luanlima.oliveir@gmail.com")
+                .mensagem("Olá Luan, bem-vindo(a) ao Gmail!")
+                .idTipoComunicacao(1)
+                .descricaoTipoComunicacao("email")
+                .build();
+
+        LogEnvioMensagem log1 = LogEnvioMensagem.builder()
+                .id(1)
+                .agendamento(agendamentoDTO.toModel())
+                .dataProcesaamento(LocalDateTime.now())
+                .statusEnvio(
+                    StatusEnvio.builder()
+                    .id(2)
+                    .descricao("error")
+                    .codigo(500)
+                    .build()
+                ).build();
+
+        LogEnvioMensagem log2 = LogEnvioMensagem.builder()
+                .id(2)
+                .agendamento(agendamentoDTO.toModel())
+                .dataProcesaamento(LocalDateTime.now())
+                .statusEnvio(
+                    StatusEnvio.builder()
+                    .id(1)
+                    .descricao("success")
+                    .codigo(200)
+                    .build()
+                ).build();
+
+        when(agendamentoService.consultarAgendamento(71)).thenReturn(Arrays.asList(log1, log2));
+
+        this.mockMvc
+                .perform(
+                        get("/api/v1/agendamento/{idAgendamento}", 71)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*]", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].idAgendamento", is(71)))
+                .andExpect(jsonPath("$[0].codigoStatus", is(500)))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].idAgendamento", is(71)))
+                .andExpect(jsonPath("$[1].codigoStatus", is(200)));
     }
 }
