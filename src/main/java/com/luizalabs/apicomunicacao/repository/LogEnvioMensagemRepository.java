83426e132d6d0824622ae64a0fe0650c7faf562f
Luan Lima <luanlima.olliveir@gmail.com>
2021-06-15 22:06:04 -0300

    Criando repository para entidade LogEnvioMensagem.

---
+++ src/main/java/com/luizalabs/apicomunicacao/repository/LogEnvioMensagemRepository.java
@@ -0,0 +1,12 @@
+package com.luizalabs.apicomunicacao.repository;
+
+import com.luizalabs.apicomunicacao.entity.LogEnvioMensagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LogEnvioMensagemRepository extends JpaRepository<LogEnvioMensagem, Integer> {
+    List<LogEnvioMensagem> findAllByAgendamento_Id(Integer idAgendamento);
+}
